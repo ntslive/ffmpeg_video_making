e3dd9e5bb4a06131058101f3a38398e82fd63c34
Jamie Burke <jjaburke@hotmail.com>
2018-09-21 15:30:14 +0100

    Tidied code up including some error handling. Added -t param.

--- storymaker.sh
+++ storymaker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# IMAGE_PATH=$1
-# LIVE_AUDIO_STREAM="https://stream-relay-geo.ntslive.net/stream?t=1535983749542"
 OUT_AUDIO_FILENAME="out_audio.mp3"
 
+# Reading params from call, getting video or image path.
 audio_path="https://stream-relay-geo.ntslive.net/stream?t=1535983749542"
-
+live_recording_length_s=15
 while test $# -gt 0; do
     case "$1" in
         -a)
@@ -18,6 +17,16 @@ while test $# -gt 0; do
             image_path=$1
             shift
             ;;
+        -v)
+            shift
+            video_path=$1
+            shift
+            ;;
+        -t)
+            shift
+            live_recording_length_s=$1
+            shift
+            ;;
         *)
             echo "$1 is not a recognized flag!"
             return 1;
@@ -25,17 +34,37 @@ while test $# -gt 0; do
     esac
 done
 
-echo $image_path
-echo $audio_path
-# ffmpeg -loop 1 -i $IMAGE_PATH -i $AUDIO_PATH -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest output.mp4
-# ffmpeg -i $LIVE_AUDIO_STREAM -c copy -flags +global_header -f segment -segment_time 5 -segment_format_options movflags=+faststart -reset_timestamps 1 test%d.mp4
+echo "audio source =" $audio_path
+if [ $video_path ]; then
+  echo "video source =" $video_path
+fi
+if [ $image_path ]; then
+  echo "image source =" $image_path
+fi
 
-# Records live stream:
-ffmpeg -y -i $audio_path -t 00:00:15 output_audio.mp3 &&
+if [ -z $video_path -a -z $image_path]; then
+  echo "\nError: Please add a video (-v PATH) or image (-i PATH)."
+  exit 1
+fi
 
-# Merges input video and replaces it's audio with output_audio.mp3
-ffmpeg -i video/ld_sample.mp4 -i output_audio.mp3 -c:v copy -tune stillimage -c:a aac -b:a 320k -pix_fmt yuv420p -map 0:v:0 -map 1:a:0 output.mp4
+#
+#
 
-# Merges image and audio file together
-# ffmpeg -y -loop 1 -i $image_path -i output_audio.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest output.mp4
-# ffmpeg -y -loop 1 -i $image_path -i $audio_path -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -aframes 5 -shortest output.mp4
+# Records live stream
+ffmpeg -y -i $audio_path -t 00:00:$live_recording_length_s output_audio.mp3 &&
+
+#
+#
+
+# Replaces audio on given video path with live stream recording.
+if [ $video_path ]; then
+  echo "video source =" $video_path
+
+  ffmpeg -i $video_path -i output_audio.mp3 -c:v copy -tune stillimage -c:a aac -b:a 320k -pix_fmt yuv420p -map 0:v:0 -map 1:a:0 output.mp4
+fi
+
+# Replaces audio on given video path with live stream recording.
+if [ $image_path ]; then
+  ffmpeg -y -loop 1 -i $image_path -i output_audio.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest output.mp4
+  # ffmpeg -y -loop 1 -i $image_path -i $audio_path -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -aframes 5 -shortest output.mp4
+fi
